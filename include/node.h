df4296f86ba9a461966749fffd67c78e5b556546
Joshua Hill <posixninja@gmail.com>
2011-03-08 15:51:45 -0500

    filled out node a little more

--- include/node.h
+++ include/node.h
@@ -8,5 +8,15 @@
 #ifndef NODE_H_
 #define NODE_H_
 
+struct list_t;
+
+typedef struct node_t {
+	unsigned int depth;
+	struct node_t* parent;
+	struct list_t* children;
+} node_t;
+
+void node_destroy(node_t* node);
+node_t* node_create(node_t* parent);
 
 #endif /* NODE_H_ */
--- node.c
+++ node.c
@@ -4,9 +4,21 @@
  *  Created on: Mar 7, 2011
  *      Author: posixninja
  */
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "node.h"
 
-int one() {
-	return 1;
+void node_destroy(node_t* node) {
+	if(node) {
+		free(node);
+	}
+}
+
+node_t* node_create(node_t* parent) {
+	node_t* node = (node_t*) malloc(sizeof(node_t));
+	if(node == NULL) {
+		return NULL;
+	}
+	return node;
 }
